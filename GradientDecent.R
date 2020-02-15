210b415516d95359e46a4ba07dc0e7b08a0f438c
Andrew Hurst <andrew_hurst92@yahoo.com>
2020-02-14 23:22:32 -0700

    plot of error rate and mean log loss

--- GradientDecent.R
+++ GradientDecent.R
@@ -20,36 +20,42 @@ spam.test <- spam[ c((spam.train.size + 1) : spam.test.size),]
 spam.val <- spam[ c((spam.test.size+1) : nrow(spam)),]
 
 #### Formatting Input/Output Matrices
-
+spam.input.list <- list(  )
 spam.train.y <- spam.train[, ncol(spam)]
 spam.train.X <- spam.train[, c(1:ncol(spam)-1) ]
+spam.input.list$train <- matrix(spam.train.X , nrow(spam.train.X) , ncol(spam.train.X))
 
 spam.test.y <- spam.test[, ncol(spam)]
 spam.test.X <- spam.test[, c(1:ncol(spam)-1) ]
+spam.input.list$test <- matrix(spam.test.X ,nrow(spam.test.X) , ncol(spam.test.X) )
 
 spam.val.y <- spam.val[, ncol(spam)]
 spam.val.X <- spam.val[, c(1:ncol(spam)-1) ]
+spam.input.list$val <- matrix(spam.val.X , nrow(spam.val.X) ,  ncol(spam.val.X) )
 
 ### create alt labels ( y hat )
  # spam
+spam.output.list <- list()
+
 spam.train.y[spam.train.y > 0] <- 1
 spam.train.y[spam.train.y < 0] <- -1
+spam.output.list$train <- matrix(spam.train.y  )
 
 spam.val.y[ spam.val.y > 0 ] <- 1 
 spam.val.y[ spam.val.y < 0 ] <- -1 
+spam.output.list$val <- matrix(spam.val.y )
 
 spam.test.y[ spam.test.y > 0 ] <- 1 
 spam.test.y[ spam.test.y < 0 ] <- -1 
-
-
+spam.output.list$test <- matrix(spam.test.y )
 #### Gradient Decent fucntion ####
 
 ## test values
-X <- matrix(spam.train.X , nrow(spam.train.X) , ncol(spam.train.X))
-y <- matrix(spam.train.y)
-X.val <- matrix( spam.val.X , nrow(spam.val.X) , ncol(spam.val.X))
-stepSize <- .001
-maxIteration <- 100
+X <- matrix(spam.input.list$train , nrow(spam.input.list$train) , ncol(spam.input.list$train))
+y <- matrix(spam.output.list$train)
+X.val <- matrix( spam.input.list$val , nrow(spam.input.list$val) , ncol(spam.input.list$val))
+stepSize <- .1
+maxIteration <- 999
 
 ##
 
@@ -64,18 +70,18 @@ GradientDescent <-function( X , y , stepSize , maxIterations){
     weightMatrix <- weightVector
     
   # for loop through 1 to max iterations
-    maxIteration.vec <- seq(1 , maxIteration, 1)
+    maxIteration.vec <- seq(1 , maxIteration , 1)
     
     for( i in maxIteration.vec ){
       
       # compute gradient given current weightVector
         # use function compute gradient over all trianing data
       
-      grad = -(y[i,] %*%  X[i,]) / ( 1 + exp( y[i,] *  as.numeric(t(weightVector) %*% X[1,])))
+      grad = -(y[i,] %*%  X[i,]) / ( 1 + exp( y[i,] *  as.numeric(t(weightVector) %*% X[i,])))
       
       
       #update the weightVector by taking a step in the negative gradient decent
-      weightVector <- weightVector + t( .01 * grad )
+      weightVector <- weightVector - t( .01 * grad )
       
       #store in the resulting weightVector in corresponding col of weightMatrix
       weightMatrix <- cbind(weightMatrix, weightVector)
@@ -89,36 +95,59 @@ GradientDescent <-function( X , y , stepSize , maxIterations){
 }
 
   ## call Gradient Decent funciton on trian data to compute a learned weightMatrix
-
+  wm <- GradientDescent(X , y , stepSize , maxIteration)
+  
    
   ## multiply train and validation inputs with weightMatrixto obtain matrix of prediction values
     # # of rows = observations , # of cols = number iterations
+  pred.m <- list()
+  pred.m$train <- X %*% wm
+  pred.m$val <- X.val %*% wm
 
-
-
+  graph.list <- list()
+  
+  metric.funs <- list(
+    mean.log.loss = function( pred.mat , y.hat){
+      colMeans( log( 1 + exp(-t(y.hat) %*% pred.mat)))
+    },
+    error.rate = function( pred.mat , y.hat){
+      pred.mat <- ifelse(pred.mat < 0 , -1 , 1)
+      100 * colMeans( y.hat != pred.mat )
+    })
+  
+  for( split in c( "train" , "val")){
+   for( fun.name in names(metric.funs) ){
+     this.fun <- metric.funs[[fun.name]]
+     metric.vec <- this.fun( pred.m$split , spam.output.list$split )
+     graph.list[[paste(split , fun.name)]] <- data.table(
+       split , fun.name , value = metric.vec ,
+       iteration = seq_along(metric.vec)
+     )
+   } 
+  }
+  
+  graph.dt <-do.call(rbind , graph.list)
+  min.dt <- metric.dt[ , data.table( what = "min" , .SD[which.min(value)]) ,
+                       by = .(split , fun.name)]
+  
+  ggplot() + geom_line( aes( iteration , value , color = split ) , data = graph.dt) +
+    geom_point( aes( iteration , value , color = split , fill=what), shpae = 21, data= min.dt) +
+    scale_fill_manual(values=c(min="white"))+
+    scale_color_manual(values=c(train="black", val="red"))+
+    theme_bw()+
+    theme(panel.margin=grid::unit(0, "lines"))+
+    facet_grid(fun.name ~ ., scales="free")
+  
+  
+    
+  
  #### GRAPHS  #######
 
    ## Plot error rate ( % incorrectly predicted labels) & log loss as function of # iterations
-
-   PrecentError <- function( maxIterations, step, X , Y){
      
      ### compute weightMatrix with size of iterations from 1 to max
-     iter.vec <- seq(1 , maxIteration, 1)
-     weightMatrix.vec <- vector()
      
-     for( i in iter.vec){
-        err.list <- list()
-        wm<-GradientDescent( X , y , step , i )
-        pred.m <- wm %*% X
-     }
-     
-     
-     
-     ### compute the error rate for each size of weight matrix
-     
-     ### return all error rates as a function of iterations
-   } 
-
+   
      ## include legend or direct labels for differentiation
 
   ## plot a point to highligh the minimum value of each of the two curves
